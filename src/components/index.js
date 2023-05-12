b9f58e167fdf541d43f41c9d862254cb691998a6
rafael <rafaelreyesgarcia93@gmail.com>
2023-05-12 13:13:50 -0500

    responsive site SPA

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Feedback from './Feedback';
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+
+export {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  Feedback,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials
+};
+
